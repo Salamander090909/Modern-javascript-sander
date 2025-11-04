fb81665939aa8a151695d51370838c87dc0c7c9c
Sander <sandertennstrand@gmail.com>
2025-11-04 13:35:12 +0100

    ferdig med oppgave 2

--- sander.js
+++ sander.js
@@ -1,9 +1,18 @@
-console.log("Hello world");
+console.log("oppgave 1: det er å sette et nytt navn på en datatype slik at den vet lagringssted for en verdi.");
+alder = 30;
 
-console.log("Hei, jeg liker å skrive js");
+console.log("oppgave 2: Å initiere en variabel en standarverdig, ved bruk av var,let,const.");
+let navn = "Sander";
 
-console.log("Oppgave 3: Det er når man setter sammen flere variabler til en streng ved hjelp av + tegnet.");
+console.log("oppgave 3")
+let alderSander = 10 + 6;
+console.log(alderSander);
 
-console.log("Oppgave 4: Det vil stå rett ut det det står. Vis poenget var å vise hvor gammel man var og alder burde man skrive det slik");
+console.log("oppgave 4: var er en gammel verson for å lage en variabel, let er en nyere verson som er mer sikker.")
 
-console.log("Geir Hilmersen er født i året " + 1979 + " og er " + 42 + " år gammel!");
+console.log("oppgave 5: En variabel er som var og let, det kan endres på. Det kan ikke konstant sånn som const. ");
+
+console.log("oppgave 6: string, number, null, undefined, boolean.");
+
+console.log("oppgave 7a: null som er definert som null, mens undefined er en variabel som ikke er definert.");
+console.log("oppgave 7b: dette kan være nyttig vis man vil sjekke om en variabel er definert eller ikke.");
