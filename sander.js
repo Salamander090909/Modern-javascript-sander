db6c37dbc99c337679e9fbe16708d95bc2ab7318
Sander <sandertennstrand@gmail.com>
2025-11-04 14:20:53 +0100

    endring oppgave 3

--- sander.js
+++ sander.js
@@ -1,18 +1,3 @@
-console.log("oppgave 1: det er å sette et nytt navn på en datatype slik at den vet lagringssted for en verdi.");
-alder = 30;
-
-console.log("oppgave 2: Å initiere en variabel en standarverdig, ved bruk av var,let,const.");
-let navn = "Sander";
-
-console.log("oppgave 3")
-let alderSander = 10 + 6;
-console.log(alderSander);
-
-console.log("oppgave 4: var er en gammel verson for å lage en variabel, let er en nyere verson som er mer sikker.")
-
-console.log("oppgave 5: En variabel er som var og let, det kan endres på. Det kan ikke konstant sånn som const. ");
-
-console.log("oppgave 6: string, number, null, undefined, boolean.");
-
-console.log("oppgave 7a: null som er definert som null, mens undefined er en variabel som ikke er definert.");
-console.log("oppgave 7b: dette kan være nyttig vis man vil sjekke om en variabel er definert eller ikke.");
+console.log("Oppgave 1: Det vil skrives 10, men siden det er skrevet med const, vil ikke variabelen endre seg.");
+const i = 10;
+console.log(`i = ${i}`);
