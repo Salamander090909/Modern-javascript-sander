a014d04999e2ad1afee7d63b130bdd11f06daf65
Sander <sandertennstrand@gmail.com>
2025-11-04 12:45:40 +0100

    ferdig med oppgavene

---
+++ sander.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+
+console.log("Hei, jeg liker å skrive js");
+
+console.log("Oppgave 3: Det er når man setter sammen flere variabler til en streng ved hjelp av + tegnet.");
+
+console.log("Oppgave 4: Det vil stå rett ut det det står. Vis poenget var å vise hvor gammel man var og alder burde man skrive det slik");
+
+console.log("Geir Hilmersen er født i året " + 1979 + " og er " + 42 + " år gammel!");
