222460743781ce1aa455edd2f5c80d320a3c7b93
Sander <sandertennstrand@gmail.com>
2025-11-18 12:50:49 +0100

    ferdig med oppgavesett-5a

--- sander.js
+++ sander.js
@@ -1,84 +1,75 @@
-// Sander oppgavesett 4
-console.log("Sander, oppgavesett 4");
+// Sander oppgavesett 5
+console.log("Sander, oppgavesett 5");
 
 
-//oppfgave 1
-console.log(1 === 1)
-console.log(1 === 2)
-console.log(3 === 3)
-console.log(2 === 3)
-// denne vil nå skjekke om verdien er lik, på de som er tre er lik tegn vil den også skjekke om data typen er lik. Dereter vil den printe ut false eller true.
+//Oppgave 1
+{
+const i = 10;
 
-//oppgave 2
-console.log(1 == '1')
-console.log(2 === '2')
-// her vil den første printe ut true fordi den skjekker bare verdien, mens den andre vil printe ut false fordi den skjekker både verdi og datatype.
-
-//oppgave 3
-console.log(1==1 && 2==2) //denne blir true fordi den bare skjekker verdien og de er like
-console.log(1==='1' && 2===2) //denne blir false fordi den skjekker både verdi og datatype og de er ikke like
-console.log(1===1 && '2' ===2) //denne blir false fordi den skjekker både verdi og datatype og de er ikke like
-console.log(1=='1' && 2=='2') //denne blir true fordi den bare skjekker verdien og de er like
-
-//oppgave 4
-console.log(1==='1' || 2===2) //denne blir true fordi den skjekker både verdi og datatype og de er ikke like
-console.log(2=='2' || 1==='1') //denne blir true fordi den bare skjekker verdien og de er like
-console.log(2===2 || 1===1 || 3==='3') //denne blir true fordi den skjekker både verdi og datatype og de er like
-
-//oppgave 5
-console.log(1 !== 1) //denne blir false fordi den skjekker verdi og de er like
-console.log(2 !== 1) //denne blir true fordi den skjekker verdi og de er ikke like
-console.log(2 !== 1 && 1 === 1) //denne blir true fordi den skjekker verdi og de er ikke like
-console.log(2 !== '2' && 1=== '1' || 2=='2' ) //denne blir true fordi den skjekker verdi og de er ikke like
+console.log(`i = ${i}`);
+}
+//den vil bytte ut {i} med verdien av i, som er 10
 
 
-//oppgavesett-4 b
-console.log("Sander, oppgavesett 4b");
-
-//oppgave 1
-console.log(5 > 3) //denne blir true fordi 5 er større enn 3
-console.log(5 < 3) //denne blir false fordi 5 ikke er mindre enn 3
-console.log(10 > 10) //denne blir false fordi 10 ikke er større enn 10
-console.log(10 >= 10) //denne blir true fordi 10 er lik 10
-console.log(8 <= 12) //denne blir true fordi 8 er mindre enn 12
-
-//oppgave 2
-console.log(4 >= 5) 
-console.log(7 <= 7)
-console.log(9 <= 4)
-console.log(6 > 2)
-// alle disse vil printe ut enten true eller false basert på sammenligningen mellom tallene. Når det står <= eller >= så betyr det at tallene kan være like også.
-
-
-//oppgave 3
-console.log(5 == 5) //denne blir true fordi 5 er lik 5
-console.log(5 == '5') //denne blir true fordi verdien er lik selv om datatypen er forskjellig.
-console.log(5 === '5') //derfor vil denne bli false siden den skjekker både verdi og datatype, som ikke er lik.
-console.log(5 != 4) //denne blir true fordi 5 ikke er lik 4
-console.log(5 !== '5') //denne blir true fordi den skjekker både verdi og datatype, som ikke er lik.
-
-
-//oppgave 4
-console.log(5 > 3 && 10 > 5)
-console.log(5 > 3 && 10 < 5)
-console.log(7 >= 7 && 2 < 5)
-console.log(4 < 2 && 3 > 1)
-//for at en && skal bli 
+//Oppgave 2
+const x = 10;
+{
+console.log(`i = ${x}`);
+}
+//den vil bytte ut {x} med verdien av x, som er 10. Så det vil stå i = 10 i konsollen.
 
 
 
-//oppgave 5
-console.log(5 > 3 || 10 > 5)
-console.log(5 > 3 || 10 < 5)
-console.log(7 < 7 || 2 < 1)
-console.log(4 < 2 || 3 > 1)
-//for at alle her skal bli true trenger en av opprasjonene å være true.
+//Oppgave 3
+{
+const y = 10;
+}
+
+console.log(`y = ${y}`);
+//den vil ikke skrive noe i konsollen fordi y er definert utenfor blokken, og er inne i en parantes. Derfor er y ikke tilgjengelig utenfor blokken, og det vil føre til en
 
 
-//oppgave 6
-console.log(5 > 3 && 10 > 5 || 2 === 2)
-console.log(4 < 2 && 3 > 1 || 10 === 10)
-console.log(5 === 5 && 3 > 10 || 8 < 12)
-//her vil den først skjekke && delen, det er fordi den står først. Også sjekker den resten. Vis man hadde brukt parantes kunne dette ha påvirket svarert.
+//Oppgave 4
+let z;
+{
+z = 10;
+
+//utskrift a)
+console.log(`z = ${z}`);
+}
+
+//utskrift b)
+console.log(`z = ${z}`);
+
+//utskriften til a vil vise z = 10, dette er fordi at z er inisaliert og deklarert. 
+//utskriften til b vil også vise z = 10, dette er fordi z blir initalisert utenfor parantesen.
+
+
+//Oppgave 5
+let a;
+fubar();
+console.log(`a = ${a}`);
+
+function fubar(){
+a =2;
+}
+//den initialiserer a med verdien 2. men den vil printe not defined fordi a ikke ble definert først.
+
+
+//Oppgave 6
+let b = 10;
+
+add(b, 5);
+console.log(b);
+
+function add(c, d){
+
+let result = c + d;
+console.log(result);
+}
+//jeg er usikker på hva den gjør, add er en funksjon og det er alt jeg vet.
+//den gir to utskrifter, den første er b pluss 5 og den andre er resultate av c+d.
+
+
 
 
