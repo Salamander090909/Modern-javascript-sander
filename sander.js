1e1e7b7b7814e8888615ee73a5aac4a97392510c
Sander <sandertennstrand@gmail.com>
2026-01-08 13:02:54 +0100

    ferdig med alle oppgavene på oppgavesett 8

--- sander.js
+++ sander.js
@@ -1,125 +1,203 @@
-console.info('opggavesett 7-1')
+console.info('Oppgavesett 8-1')
 
-/* oppg 1
-a) arrays er en enklere måte å sammle mange tall, dette er praktisk når man skal endre på ting senere som for eksempel en poengsum.
-b) Det som er forskelig fra andre kodespråk er at en variabel kan holde mange verdier, de ligger i en fast rekkefølge og at man henter de ved å bruke plaseringen deres. Den første plasseringen i en rekke starter altid på 0
-*/
+//oppg 1
+/*
+For lopps
+Brukes når man vet hvor mange ganger man vil at koden skal kjøre.
 
-// oppg 2 a
-
-const liste = [];
-
-//b
-const ender = ["Donald Duck", "Ole", "Dole", "Doffen", "Skrue McDuck", "Petter Smart", "Bestemor Duck", "Naboen", "Guffen", "Magica"];
+Den har tre deler: Start, betingelse og hva som skjer etter hver rundt */
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
 
 
 
-//c
-console.log(ender[2], ender[5], ender[8])
+
+/*
+While loops
+denne kjører så lenge en betingelse er true. Her må man passe på at man ikke lager en evig loop.
+
+Den funker ved å sjekke betingelsen før hver runde */
+let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+
+
+
+/*
+Do While Lopps
+Den er veldig lik while loop, men den kjører altid minst en gang.
+
+Den funker ved og først kjøre koden også sjekke etter betingelsen. */
+
+let a = 10;
+
+do {
+  console.log(i);
+} while (i < 5);
+
+
+
+
+/*
+For...of-loop
+Brukes for å gå gjennom verdier i arrays, sting osv.
+
+Den funker ved å hente hvert element direkte, uten indeks. */
+
+const tall = [2, 4, 6, 8];
+
+for (const nummer of tall) {
+  console.log(nummer);
+}
+
+
+
+
+/*
+For...in-loops
+Brukes mest for objekter. Den går gjennom keys, ikke verdier.
+
+Den funker ved å loppe gjennom navnene på egenskapene i et object. */
+
+const person = {
+  navn: "Sander",
+  alder: 16,
+  by: "Asker"
+};
+
+for (const key in person) {
+  console.log(key + ": " + person[key]);
+}
+
+
+
+//oppg 2
+
+const navneliste = ['Geir', 'Espen', 'Ellen', 'Erik', 'Lars', 'Gunnar', 'Oda', 'Nina', 'Tine', 'Henrik', 'Agnethe'];
+
+for (let navn of navneliste) {
+    console.log(navn + ' har ' + navn.length + ' bokstaver');
+}
+
+// jeg valgte denne loopen fordi den er lett å lese, trenger ikke å tenke på index = 0, og det passer perfekt når man har en liste som dette.
 
 
 //oppg 3
-//a) index er plasseringen til en verdi i en array. Så donald duck vil ha index 0 i dette tilfelle.
 
-//b første index i en array er altid 0
+const snuddListe = [];
 
-//c) det finnes egentlig ikke noe fast slutt på array, men indexen til den siste i rekken er bare plasseringen -1
+for (let i = navneliste.length - 1; i >= 0; i--) {
+    snuddListe.push(navneliste[i]);
+}
 
-//d) array.length er hvor lang rekken er, så vis det er tre elementer i arrayen, vil length være 3.
-
-//e) siden index starter på 0, vil altid array.length -1 være verdien av indexen.
+console.log(snuddListe);
 
 
 //oppg 4
-console.log(typeof ender)
-
-//a) vis man bruker typeof på en array vil det bli object som man kan se.
-
-//b) for å sjekke om en variabel eller en konstant er en array bruker man 'array.isArray()'. Den vil da returnere true vis det er en array.
-
-//c)
-console.log(sjekkArray(ender));
+console.log(reduceToOddNumber(30));
 
 
-function sjekkArray(verdi) {
-    if (Array.isArray(verdi)) {
-        return `Argumente er en array`;
-    } else {
-        return `Argumente er ikke en array`
+function reduceToOddNumber(number) {
+
+    if (typeof(number) === 'number') {
+
+        while (number % 2 === 0) {
+            number = number / 2;
+        }
+
+        return number;
     }
 }
 
 
-console.log('Oppgavesett-7-2')
+//oppg 5
+/* jeg har ikke brukt do...while og for...in. Jeg kunne brukt do...while på oppg4 og do...while kunne jeg nok brukt på navnelisten. */
+
+
+
+console.info('Oppgavesett 8-2')
 
 //oppg 1
-const tall = [10, 20, 30]
+let navn1 = ["Odin", "Thor", "Loki", "Freya", "Baldur", "Frigg", "Tyr", "Heimdall", "Hel", "Idunn", "Njord", "Skadi", "Fenrir", "Jormungandr", "Sleipnir"];
 
-console.log(tall[2])
-
-//oppg 2
-const helloWorld = [];
-
-helloWorld[0] = 'hello';
-helloWorld[1] = 'world';
-
-console.log(helloWorld)
-
-//oppg 3
-const lengde = [1, 2, 3, 4, 5];
-
-console.log(lengde.length)
-
-//oppg 4
-const frukt = ["apple", "banana", "cherry"]
-
-frukt[1] = "blueberry";
-
-console.log(frukt)
-
-//oppg 5
-const tallRekke = [5, 10, 15]
-
-tallRekke[3] = 20;
-
-console.log(tallRekke);
-
-
-//oppg 6
-const farger = ['red', 'green', 'blue']
-
-console.log(farger[farger.length - 1])
-
-//oppg 7
-const syvGangen = [7, 14, 21];
-
-if (syvGangen[0] < syvGangen[2]) {
-    console.log('true')
-} else {
-    console.log('false')
+for (let i = 0; i < navn1.length; i++) {
+    console.log(navn1[i]);
 }
 
 
-//oppg 8
-const toGangen = [2, 4, 6, 8]
+//oppg 2
 
-console.log(toGangen[0], toGangen[2])
+let tallRekke = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30];
+let sum = 0;
+
+for (let i = 0; i < tallRekke.length; i++) {
+    sum += tallRekke[i];
+}
+
+console.log(sum);
 
 
-//oppg 9
-const SubArrays = [
-    [1, 2, 3],
-    [4, 5, 6]
+//oppg 3a
+
+let navn = [
+    "Anne", "Jan", "Per", "Bjørn", "Ole", "Lars", "Inger", "Kjell", "Kari",
+    "Thomas", "Ingrid", "Svein", "Marit", "Knut", "Tor", "Liv", "Geir",
+    "Martin", "Hans", "Morten", "Andreas", "Erik", "Ida", "Arne", "Maria",
+    "Anna", "Terje", "Ann", "Eva", "Daniel", "Anders", "John", "Rune",
+    "Odd", "Hilde", "Nina", "Marianne", "Magnus", "Kristin", "Eli", "Ola",
+    "Siri", "Einar", "Gunnar", "Randi", "Bente", "Johan", "Mona", "Trond",
+    "Hege", "Espen", "Siv", "Steinar", "Elin", "Kjetil", "Linda", "Harald",
+    "Grete", "Leif", "Anita", "Roar", "Tone", "Jon", "Camilla", "Stig",
+    "Lene", "Arild", "Gry", "Frode", "Ragnhild", "Pål", "Britt", "Øyvind",
+    "Laila", "Vidar", "Ellen", "Dag", "Sissel", "Knut", "Anne", "Per",
+    "Inger", "Bjørn", "Kari", "Ole", "Lars", "Jan", "Marit", "Thomas",
+    "Ingrid", "Mohammed", "Ali", "Ahmed", "Aisha", "Fatima", "Hassan",
+    "Sara", "Yousef", "Amira", "Omar"
 ];
 
-console.log(SubArrays[1][1])
+function tellNavn(navn, array) {
+    let antall = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === navn) {
+            antall++;
+        }
+    }
+
+    console.log(`${navn} er oppført ${antall} ganger i arrayen som er ${array.length} lang`);
+}
 
 
-//oppg 10
-const x = 100;
-const y = 200;
-const z = 300;
+tellNavn("Arne", navn);
 
-const variabel = [x, y, z];
 
-console.log(variabel);
+
+
+//oppg 3b
+
+
+function tellNavn(navn, array) {
+    let antall = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === navn) {
+            antall++;
+        }
+    }
+
+    console.log(`${navn} er oppført ${antall} ganger i arrayen som er ${array.length} lang`);
+}
+
+function tellAlleNavn(array) {
+    for (let i = 0; i < array.length; i++) {
+        tellNavn(array[i], array);
+    }
+}
+
+tellAlleNavn(navn);
+
